5ee37ba3c2e9ca775b8ad07d5a02f0c051c06903
AnishMandal939 <mandalktaanees12@gmail.com>
2024-02-08 07:41:06 +0545

    feat - Add ResponseEntity to JobController methods, for response status handling

--- src/main/java/com/jobFinder/First/job/application/job/JobController.java
+++ src/main/java/com/jobFinder/First/job/application/job/JobController.java
@@ -1,5 +1,7 @@
 package com.jobFinder.First.job.application.job;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,20 +16,20 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+        return ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<>("Job added successfully",HttpStatus.CREATED);
     }
     @GetMapping("/jobs/{id}")
-    public Job getJobNyId(@PathVariable Long id){
+    public ResponseEntity<Job> getJobNyId(@PathVariable Long id){
         Job job = jobService.getJobById(id);
         if(job != null)
-            return job;
-        return new Job(1L,"TestJob", "TestJobdesc", "20000", "25000", "location");
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
