1de4ca7a5e3339fa1f9ed85ef353621da07db935
AnishMandal939 <mandalktaanees12@gmail.com>
2024-02-08 06:20:48 +0545

    first commit

---
+++ src/main/java/com/jobFinder/First/job/application/FirstJobApplication.java
@@ -0,0 +1,13 @@
+package com.jobFinder.First.job.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobFinder/First/job/application/FirstJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobFinder.First.job.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
